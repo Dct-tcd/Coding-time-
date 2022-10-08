fe293979c82c1549664359331afab2dd1c4e7993
Dev <tcd16112002@gmail.com>
2022-10-08 19:34:17 +0530

    Small fix

--- script.js
+++ script.js
@@ -6,50 +6,27 @@ function myFunctionall()
     api_url="https://www.kontests.net/api/v1/all";
     getapi(api_url,0);
 }
-// function newtime1(time)
-// {
-//     // let longMonth = time.toLocaleString('en-us', { month: 'long' }); /* June */
-//     // return longMonth;
-//     // console.log(longMonth);
-//     let r="",p="",ans=0;
-//     let  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//    time.forEach(Element => {
-    // if (Element=='-') ans++;
-    // if (ans==0) r+=Element;
-//    });
-// for(let i=0;i<time.length;i++)
-// {
-//     if (ans==1) p+=time[i];
-//     if (time[i]=='-') ans++;
-//     if (ans==0) r+=time[i];
-// }
-//    return  r + " " +months[p];
-// }       
 function myFunctioncf()
 {
-    // console.log("1");
-    // var target = document.getElementById("cf");
-    // target.innerHTML += "<style>color:green</style>";
-    // {<style>color:blue</style>}
     api_url="https://www.kontests.net/api/v1/codeforces";
     getapi(api_url,1);
 }  function myFunctionlc()
 {
     api_url="https://www.kontests.net/api/v1/leet_code";
-    getapi(api_url,1);
+    getapi(api_url,2);
 }  function myFunctionatr()
 {
     api_url="https://www.kontests.net/api/v1/at_coder";
-    getapi(api_url,1);
+    getapi(api_url,3);
 }  function myFunctioncc()
 {
     api_url="https://www.kontests.net/api/v1/code_chef";
-    getapi(api_url,1);
+    getapi(api_url,4);
 }  
 function myFunctionkick()
 {
     api_url="https://www.kontests.net/api/v1/kick_start";
-    getapi(api_url,1);
+    getapi(api_url,5);
 }  
 async function getapi(url,apo)
  {
@@ -101,17 +78,14 @@ if (r.site!="HackerRank"&&r.site!="HackerEarth")
     let dt1 = new Date(r.end_time);
     r.start_time = dt.toLocaleString();
     r.end_time = dt1.toLocaleString();
-    
      r.url=str.link(r.url);
-    
-     //  r.url={r.name};
-    // let a = document.createElement('a');
-    // var linkText = document.createTextNode(r.name);
-    // a.appendChild(r.name);
-    // a.title = "my title text";
-    // a.href = r.url;
-    // document.body.appendChild(a);
-    // let pp=<tr><td><a href="r.url">r.name</a></td></tr>;
+     let siteo=r.site;
+     if (apo==1) siteo="Codeforces";
+     if (apo==2) siteo="LeetCode";
+     if (apo==3) siteo="Atcoder";
+     if (apo==4) siteo="Codechef";
+     if (apo==5) siteo="KickStart";
+     if (apo==0) siteo=r.site; 
       tab += 
     `<tr> 
     <td>${r.name} </td>
@@ -120,7 +94,7 @@ if (r.site!="HackerRank"&&r.site!="HackerEarth")
     <td>${r.start_time}</td>
     <td>${r.end_time}</td>
     <td>${r.duration}</td>
-    <td>${r.site}</td>
+    <td>${siteo}</td>
     <td>${str1}</td>
     </tr>`;
         }
