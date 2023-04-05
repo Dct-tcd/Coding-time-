82fe6f353f7cdf61dad7a0127d3746ac2d506a75
Dev <tcd16112002@gmail.com>
2023-04-05 22:46:53 +0530

    Reminder problem fix

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function myFunctionall()
 // });
 
 }
-document.getElementById('1').click
 function myFunctioncf()
 {
     api_url="https://www.kontests.net/api/v1/codeforces";
@@ -49,16 +48,59 @@ function myFunctionkick()
     getapi(api_url,5);
     myFunction();
 }  
+
+// var el = document.getElementById("1");
+
+// if (el.addEventListener) {
+//     el.addEventListener("click", function() {
+//         myFunctionall();
+//     }, false);
+// } else { //IE8 support
+//     el.attachEvent("onclick", function() { 
+//         myFunctionall();
+//     });
+// }
+
+// let el = document.getElementById("2");
+
+// if (el.addEventListener) {
+//     el.addEventListener("click", function() {
+//         myFunctionall();
+//     }, false);
+// } else { //IE8 support
+//     el.attachEvent("onclick", function() { 
+//         myFunctionall();
+//     });
+// }
+// document.getElementById('1').onclick(myFunctionall());
+// document.getElementById('2').onclick(myFunctioncf());
+// document.getElementById('3').onclick(myFunctionlc());
+// document.getElementById('4').onclick(myFunctionatr());
+// document.getElementById('5').onclick(myFunctioncc());
+// document.getElementById('6').onclick(myFunctionkick());
+window.onload=function(){
+
+    document.getElementById("1").addEventListener("click", myFunctionall);
+    document.getElementById("2").addEventListener("click", myFunctioncf);
+    document.getElementById("3").addEventListener("click", myFunctionlc);
+    document.getElementById("4").addEventListener("click", myFunctionatr);
+    document.getElementById("5").addEventListener("click", myFunctioncc);
+    // document.getElementById("6").addEventListener("click", myFunctionkick);
+    
+}
+
 async function getapi(url,apo)
  {
         const response = await fetch(url);
         let data = await response.json();
-    console.log(data.size);
+        // console.log(data);
+    
+        // console.log(data.size);
     if (response) {
         hideloader();
     }
     show(data,apo);
-    showo(data,apo);
+    // showo(data,apo);
 }
 getapi(api_url);
 // function Rem_site(site)
@@ -93,32 +135,30 @@ function hideloader() {
 function show(data,apo) {
  let tab = 
         `<tr>
-        <th>Reminder</th>
-        <th>Site</th>  
-        <th>Name</th>
+          <th>Reminder</th>
+          <th>Site</th>  
+          <th>Name</th>
           <th>In_24_Hours</th>
           <th>Start time</th>
           <th>link to the contest</th>
           <th>End time</th>
-          <th>Duration in Min</th>
-          
+          <th>Duration in Min</th>    
            </tr>`;
           let tabo = 
         `<tr>
-        <th>Reminder</th>
-        <th>Site</th>
+          <th>Reminder</th>
+          <th>Site</th>
           <th>Name</th>
           <th>Start time</th>
-          <th>link to the contest</th>
-          <th>End time</th>
           <th>Duration in Min</th>
-          
           </tr>`;
- 
+//  console.log(apo);
          data.sort((a, b) => b.start_time > a.start_time ? -1: 1);
         //  let pp=0;
-      for (let r of data) { 
-if (r.site!="HackerRank"&&r.site!="HackerEarth")
+      for (let r of data)
+       { 
+        
+if (r.site==null||r.site=="CodeForces"||r.site=="CodeChef"||r.site=="AtCoder"||r.site=="LeetCode")
         { 
         r.duration = parseInt(r.duration)/60;
         let str=r.url;
@@ -126,16 +166,22 @@ if (r.site!="HackerRank"&&r.site!="HackerEarth")
         let str1=r.status;
         if (str1=="CODING")str1="Running";
         else str1="Not Yet Running";
-        
+
+
+        let str_url=r.url;    
      r.url=str.link(r.url);
+    
      let siteo=r.site;
-   
+   if (siteo=="CodeForces") siteo="Codeforces";
+   if (siteo=="CodeChef") siteo="Codechef";
+   if (siteo=="AtCoder") siteo="Atcoder";
+   if (siteo=="LeetCode") siteo="Leetcode";
     
      if (apo==1) siteo="Codeforces";
-     if (apo==2) siteo="LeetCode";
+     if (apo==2) siteo="Leetcode";
      if (apo==3) siteo="Atcoder";
      if (apo==4) siteo="Codechef";
-     if (apo==5) siteo="KickStart";
+     if (apo==5) siteo="Kickstart";
      if (apo==0) siteo=r.site;
 
 // let linker1="/";
@@ -156,9 +202,7 @@ if (r.site!="HackerRank"&&r.site!="HackerEarth")
      tem = dt1.toISOString();
      r.start_time = dt.toLocaleString();
      r.end_time = dt1.toLocaleString();
-    //  let dt1 = new Date(r.end_time);
-    //  tre = dt.toISOString();
-    //  tem = dt1.toISOString();
+     
      let ans="";
      for(let i=0;i<tre.length;i++)
      {
@@ -181,9 +225,15 @@ tem=cp;
      
 // let rem_link = linker4 + tre + linker1 + tem + linker2 + r.site + linker3 + r.url ;
 //  rem_link = str.link(rem_link);
-console.log(siteo);
-let op = `https://calendar.google.com/event?action=TEMPLATE&dates=${tre}/${tem}&text=${siteo}&location=${r.site}`;
-
+// console.log(siteo);
+// console.log(3,apo);
+let desc_str="";
+for(let i=0;i<r.name.length;i++)
+{
+ if (r.name[i]==" ") desc_str+='+';
+ else desc_str+=r.name[i];
+}
+let op = `https://calendar.google.com/event?action=TEMPLATE&dates=${tre}/${tem}&text=${desc_str}&location=${str_url}`;
      tab += 
     `<tr> 
     <td> <a href= ${op} target="_blank">📅 Reminder </a></td>
@@ -195,19 +245,17 @@ let op = `https://calendar.google.com/event?action=TEMPLATE&dates=${tre}/${tem}&
     <td>${r.end_time}</td>
     <td>${r.duration}</td>
     </tr>`;
- tabo += 
-`<tr> 
-<td> <a href= ${op} target="_blank">📅 Reminder </a></td>
-<td>${siteo}</td>
-<td>${r.name} </td>
-<td>${r.start_time}</td>
-<td>${r.url}</td>
-<td>${r.end_time}</td>
-<td>${r.duration}</td>
-</tr>`;
-        }       
+        
+     tabo += 
+    `<tr> 
+    <td> <a href= ${op} target="_blank" style="text-decoration:none;">&nbsp;&nbsp;Add📅 Reminder </a></td>
+    <td>${siteo} </td>
+    <td>${r.name} </td>
+    <td>${r.start_time}</td>
+    <td>${r.duration}</td>
+    </tr>`;
+}       
     }
-    document.getElementById("employees").innerHTML = tab ;
-    // document.getElementById("emp").innerHTML = tabo ;
-
+    if (window.screen.width>=700)  document.getElementById("employees").innerHTML = tab;
+    else  document.getElementById("employees").innerHTML = tabo ;
 }
